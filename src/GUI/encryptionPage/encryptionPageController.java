0963e49cb3d1fd4d7c8e37bdbe8461cb339d0529
fengluoqiuwu <163119756+fengluoqiuwu@users.noreply.github.com>
2024-04-30 18:11:41 +0800

    Update

--- src/GUI/encryptionPage/encryptionPageController.java
+++ src/GUI/encryptionPage/encryptionPageController.java
@@ -18,7 +18,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.security.NoSuchAlgorithmException;
 
 import static GUI.GUIUtil.StageUtil.resetLocation;
 
